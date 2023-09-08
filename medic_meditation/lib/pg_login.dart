6844de7a5bf0570837c3a6e976ca952e1cdc8eff
estacionamento <cesarvatec@hotmail.com>
2023-09-08 20:41:46 -0300

    alteracoes tela login

--- medic_meditation/lib/pg_login.dart
+++ medic_meditation/lib/pg_login.dart
@@ -3,20 +3,72 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  @override
-    Widget build(BuildContext context){
-      return Scaffold(
-         appBar:AppBar(
-         title:Text('Medic_Meditation'),
-         actions:[
-           Center(child:Text('menu'),),
-            ],
-         
-      
-         ),
-       drawer: Drawer(),
+ @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginScreen(),
     );
   }
- 
 }
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              'Bem-vindo ao Medic Free Meditation',
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'E-mail',
+              ),
+            ),
+            SizedBox(height: 20.0),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Senha',
+              ),
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                // Adicione aqui a lógica de autenticação
+                // Por exemplo, verificar as credenciais e fazer login
+              },
+              child: Text('Entrar'),
+            ),
+            SizedBox(height: 10.0),
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: () {
+                  // Adicione aqui a lógica para redefinir a senha
+                },
+                child: Text('Esqueceu a senha?'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
 
