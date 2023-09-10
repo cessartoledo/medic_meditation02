484231643cc9cce6867b36a73330ea5c2534a682
estacionamento <cesarvatec@hotmail.com>
2023-09-09 22:24:48 -0300

    alteracoes tela login cor

--- medic_meditation/lib/pg_login.dart
+++ medic_meditation/lib/pg_login.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
  @override
-  Widget build(BuildContext context) {
+ Widget build(BuildContext context) {
     return MaterialApp(
       home: LoginScreen(),
     );
@@ -13,50 +13,91 @@ class LoginPage extends StatelessWidget {
 
 class LoginScreen extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+    Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
+      backgroundColor: Color(0xFF233B45), // Cor de fundo #233B45
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Text(
-              'Bem-vindo ao Medic Free Meditation',
-              style: TextStyle(
-                fontSize: 18.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 20.0),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'E-mail',
-              ),
-            ),
-            SizedBox(height: 20.0),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Senha',
-              ),
-            ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                // Adicione aqui a lógica de autenticação
-                // Por exemplo, verificar as credenciais e fazer login
-              },
-              child: Text('Entrar'),
-            ),
-            SizedBox(height: 10.0),
-            Align(
-              alignment: Alignment.centerRight,
-              child: TextButton(
-                onPressed: () {
-                  // Adicione aqui a lógica para redefinir a senha
-                },
-                child: Text('Esqueceu a senha?'),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Text(
+                    'Bem-vindo ao Medic Free Meditation',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white, // Cor do texto do título
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'E-mail',
+                      labelStyle: TextStyle(
+                        color: Colors.white, // Cor do texto do rótulo (label)
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Senha',
+                      labelStyle: TextStyle(
+                        color: Colors.white, // Cor do texto do rótulo (label)
+                      ),
+                    ),
+                  ),
+                   SizedBox(height: 10.0),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      onPressed: () {
+                        // Adicione aqui a lógica para redefinir a senha
+                      },
+                      child: Text(
+                        'Esqueceu a senha?',
+                        style: TextStyle(
+                          color: Colors.white, // Cor do texto do botão "Esqueceu a senha?"
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Adicione aqui a lógica de autenticação
+                      // Por exemplo, verificar as credenciais e fazer login
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xFF729899), // Cor de fundo do botão #729899
+                    ),
+                    child: Text(
+                      'Entrar',
+                      style: TextStyle(
+                        color: Colors.white, // Cor do texto do botão
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Não tem uma conta? Cadastre-se",
+                      style: TextStyle(
+                        color: Colors.white, // Cor do texto do link "Não tem uma conta? Cadastre-se"
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                 
+                ],
               ),
             ),
           ],
@@ -64,11 +105,9 @@ class LoginScreen extends StatelessWidget {
       ),
     );
   }
-}
+} 
+ 
 
 
 
-
-
-
-
+  
